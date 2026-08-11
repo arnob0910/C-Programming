d476f5f10a45b6d73ca3c5df5cd484ab3d4594d2
Arnob Halder <arnobhalder0910@gmail.com>
2026-08-11 09:00:31 +0600

    test programs

---
+++ variable/1/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char characterName[] = "Arnob";
+    int characterAge = 19;
+
+    printf("There once was a man named %s\n", characterName);
+    printf("He was %d years old.\n", characterAge);
+    printf("He really liked the name %s\n", characterName);
+    printf("But did not like being %d.\n", characterAge);
+
+    return 0;
+}
