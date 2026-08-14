c4f28e65dff18dbc9e6b89c56b9bbf7c8043cf2a
Arnob Halder <arnobhalder0910@gmail.com>
2026-08-14 06:23:04 +0600

    add my practice works of C programming

---
+++ Datatypes/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 40;
+    double gpa = 3.7;
+    char grade = 'A';
+    char phrase [] = "Giraffe Academy";
+    printf("Age : %d\n",age);
+   printf("GPA : %.1f\n",age);
+  printf("Grade : %c\n",age);
+  printf("Phrase : %s\n",age);
+
+    return 0;
+}
